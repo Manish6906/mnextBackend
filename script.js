5ef05301f747933ad8f78e7fe0b9d00d13016482
Manish <139942794+Manish6906@users.noreply.github.com>
2025-08-30 18:38:05 +0530

    sdf

--- script.js
+++ script.js
@@ -10,7 +10,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middlewares
-app.use(cors());
+app.use(cors(
+    {
+        origin: "https://mnextfrontend.vercel.app", // your frontend URL
+    }
+));
 app.use(bodyParser.json());
 
 // Routes
